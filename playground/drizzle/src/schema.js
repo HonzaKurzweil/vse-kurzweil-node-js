ad84c3c292b8240274ee1fa7b9ab267bde6b44bd
Jan Kurzweil <kurj00@vse.cz>
2025-03-27 09:03:15 +0100

    playground drizzle

---
+++ playground/drizzle/src/schema.js
@@ -0,0 +1,12 @@
+import { tinytext } from "drizzle-orm/mysql-core"
+import {
+    sqliteTable,
+    int,
+    text
+} from "drizzle-orm/sqlite-core"
+
+export const todosTable = sqliteTable("todos", {
+    id: int().primaryKey({autoIncrement: true}),
+    title: text().notNull(),
+    done: int({ mode: "boolean"}).notNull()
+})
