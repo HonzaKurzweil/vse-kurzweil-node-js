5ebb7ae604ebe52f13e0b34e2a3a17d0638aa886
Jan Kurzweil <kurj00@vse.cz>
2025-04-20 15:55:47 +0200

    playground cleaned

--- playground/webSockets/drizzle.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "drizzle-kit"
-
-export default defineConfig({
-    dialect: "sqlite",
-    schema: "./src/schema.js",
-    dbCredentials: {
-        url: "file:db.sqlite"
-    }
-})
