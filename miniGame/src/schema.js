7c8bd9eacec5a69a6d65e34e7db11bca1de6f37a
Jan Kurzweil <kurj00@vse.cz>
2025-06-07 16:10:31 +0200

    db init

---
+++ miniGame/src/schema.js
@@ -0,0 +1,14 @@
+
+import {
+  sqliteTable,
+  int,
+  text,
+} from "drizzle-orm/sqlite-core"
+
+export const usersTable = sqliteTable("users", {
+  id: int().primaryKey({ autoIncrement: true }),
+  username: text().notNull().unique(),
+  hashedPassword: text().notNull(),
+  salt: text().notNull(),
+  token: text().notNull(),
+})
