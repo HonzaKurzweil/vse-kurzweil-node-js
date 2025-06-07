c12299fd6e5f1ad1203d4d4ba122c4ccd6ca266e
Jan Kurzweil <kurj00@vse.cz>
2025-06-07 16:58:30 +0200

    main page init

--- miniGame/src/app.js
+++ miniGame/src/app.js
@@ -1,13 +1,16 @@
 import { Hono } from "hono"
  import { renderFile } from "ejs"
-
+import { serveStatic } from "@hono/node-server/serve-static"
 
 
 export const app = new Hono()
 
 app.get('/', async (c) => {
     
-    const rendered = await renderFile('views/login.html',  {
+    const rendered = await renderFile('views/mainPage.html',  {
     })
     return c.html(rendered)
 })
+
+app.use('/profile_pics/*', serveStatic({ root: './' }))
+app.use('/styles.css', serveStatic({ root: './' }))
